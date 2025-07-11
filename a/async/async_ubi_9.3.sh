057a7ff3e0f29a95f864ef7d17c5cf416c058aa7
Sankalp <Sankalp@ibm.com>
2025-07-11 09:51:35 +0530

    Minor fix for packages (#25)
    * Create LICENSE
    
    * removed test from async
    
    * Update currency-build.yml

--- a/async/async_ubi_9.3.sh
+++ a/async/async_ubi_9.3.sh
@@ -41,16 +41,5 @@ if ! npm install; then
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
-fi
 
-if ! npm test; then
-    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-    exit 2
-else
-    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
-    exit 0
 fi
