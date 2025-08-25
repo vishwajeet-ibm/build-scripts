9269ed619f1d5a85cce2cb468c367210b417b91a
vishwajeet-ibm <vishwajeet.rajopadhye1@ibm.com>
2025-08-25 16:07:13 +0530

    Update grype_image_scan.sh

--- gha-script/scanner-scripts/grype_image_scan.sh
+++ gha-script/scanner-scripts/grype_image_scan.sh
@@ -5,8 +5,8 @@ build_docker=$BUILD_DOCKER
 
 if [ $build_docker == true ];then
 	 GRYPE_VERSION=$(curl -s https://api.github.com/repos/anchore/grype/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
-	 wget https://github.com/anchore/grype/releases/download/$GRYPE_VERSION/grype_${GRYPE_VERSION#v}_linux_ppc64le.tar.gz
-	 tar -xzf grype_${GRYPE_VERSION#v}_linux_ppc64le.tar.gz
+	 wget https://github.com/anchore/grype/releases/download/$GRYPE_VERSION/grype_${GRYPE_VERSION#v}_linux_s390x.tar.gz
+	 tar -xzf grype_${GRYPE_VERSION#v}_linux_s390x.tar.gz
          chmod +x grype
          sudo mv grype /usr/bin
 	 echo "Executing grype scanner"
