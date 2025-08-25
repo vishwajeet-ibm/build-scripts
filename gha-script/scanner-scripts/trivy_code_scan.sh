a9d1e00343a9c4f1d6f14c690c0977157399f495
vishwajeet-ibm <vishwajeet.rajopadhye1@ibm.com>
2025-08-25 16:05:01 +0530

    Update trivy_code_scan.sh

--- gha-script/scanner-scripts/trivy_code_scan.sh
+++ gha-script/scanner-scripts/trivy_code_scan.sh
@@ -6,8 +6,8 @@ cd package-cache
 
 if [ $validate_build_script == true ];then
 	TRIVY_VERSION=$(curl -s https://api.github.com/repos/aquasecurity/trivy/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
-	wget https://github.com/aquasecurity/trivy/releases/download/${TRIVY_VERSION}/trivy_${TRIVY_VERSION#v}_Linux-ppc64le.tar.gz
-	tar -xzf trivy_${TRIVY_VERSION#v}_Linux-ppc64le.tar.gz
+	wget https://github.com/aquasecurity/trivy/releases/download/${TRIVY_VERSION}/trivy_${TRIVY_VERSION#v}_Linux-s390x.tar.gz
+	tar -xzf trivy_${TRIVY_VERSION#v}_Linux-s390x.tar.gz
         chmod +x trivy
         sudo mv trivy /usr/bin
 	echo "Executing trivy scanner"
