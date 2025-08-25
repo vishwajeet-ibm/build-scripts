ea69ffc68259288b9be0a0cde1b594a3893a1f36
vishwajeet-ibm <vishwajeet.rajopadhye1@ibm.com>
2025-08-25 16:07:40 +0530

    Update syft_image_scan.sh

--- gha-script/scanner-scripts/syft_image_scan.sh
+++ gha-script/scanner-scripts/syft_image_scan.sh
@@ -5,8 +5,8 @@ build_docker=$BUILD_DOCKER
 
 if [ $build_docker == true ];then
         SYFT_VERSION=$(curl -s https://api.github.com/repos/anchore/syft/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
-        wget https://github.com/anchore/syft/releases/download/$SYFT_VERSION/syft_${SYFT_VERSION#v}_linux_ppc64le.tar.gz
-        tar -xzf syft_${SYFT_VERSION#v}_linux_ppc64le.tar.gz
+        wget https://github.com/anchore/syft/releases/download/$SYFT_VERSION/syft_${SYFT_VERSION#v}_linux_s390x.tar.gz
+        tar -xzf syft_${SYFT_VERSION#v}_linux_s390x.tar.gz
         chmod +x syft
         sudo mv syft /usr/bin  
         echo "Executing syft scanner"
