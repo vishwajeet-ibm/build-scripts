37e94d0678dc7397d3f920d7b74a6fbb680a2401
vishwajeet-ibm <vishwajeet.rajopadhye1@ibm.com>
2025-08-29 12:49:12 +0530

    Update trivy_image_scan.sh

--- gha-script/scanner-scripts/trivy_image_scan.sh
+++ gha-script/scanner-scripts/trivy_image_scan.sh
@@ -20,7 +20,7 @@ image_name=$IMAGE_NAME
 build_docker=$BUILD_DOCKER
 
 if [ "$build_docker" == true ]; then
-    TRIVY_VERSION=$(curl -s https://api.github.com/repos/aquasecurity/trivy/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
+    TRIVY_VERSION="v0.53.0"  # Stable release for s390x
     wget https://github.com/aquasecurity/trivy/releases/download/${TRIVY_VERSION}/trivy_${TRIVY_VERSION#v}_Linux-s390x.tar.gz
     tar -xzf trivy_${TRIVY_VERSION#v}_Linux-s390x.tar.gz
     chmod +x trivy
@@ -36,5 +36,6 @@ if [ "$build_docker" == true ]; then
     sudo trivy -q image --timeout 30m -f json ${image_name} > trivy_image_vulnerabilities_results.json
     sudo trivy -q image --timeout 30m -f cyclonedx ${image_name} > trivy_image_sbom_results.cyclonedx
 fi
+
 
 
