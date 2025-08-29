e889d3dcb0650c5d4f0573ed2c6058f3ccf60b4c
vishwajeet-ibm <vishwajeet.rajopadhye1@ibm.com>
2025-08-29 12:56:09 +0530

    Update trivy_image_scan.sh

--- gha-script/scanner-scripts/trivy_image_scan.sh
+++ gha-script/scanner-scripts/trivy_image_scan.sh
@@ -20,7 +20,8 @@ image_name=$IMAGE_NAME
 build_docker=$BUILD_DOCKER
 
 if [ "$build_docker" == true ]; then
-    TRIVY_VERSION="v0.53.0"  # Stable release for s390x
+    # Use a stable version instead of latest
+    TRIVY_VERSION="v0.53.0"
     wget https://github.com/aquasecurity/trivy/releases/download/${TRIVY_VERSION}/trivy_${TRIVY_VERSION#v}_Linux-s390x.tar.gz
     tar -xzf trivy_${TRIVY_VERSION#v}_Linux-s390x.tar.gz
     chmod +x trivy
@@ -29,12 +30,12 @@ if [ "$build_docker" == true ]; then
     echo "Cleaning old Trivy cache..."
     sudo rm -rf /root/.cache/trivy ~/.cache/trivy
 
-    echo "Updating Trivy DB..."
-    sudo trivy image --download-db-only
+    echo "Updating Trivy DB in light mode..."
+    sudo trivy image --download-db-only --light
 
-    echo "Executing Trivy scanner..."
-    sudo trivy -q image --timeout 30m -f json ${image_name} > trivy_image_vulnerabilities_results.json
-    sudo trivy -q image --timeout 30m -f cyclonedx ${image_name} > trivy_image_sbom_results.cyclonedx
+    echo "Executing Trivy scanner in light mode..."
+    sudo trivy -q image --light --timeout 30m -f json ${image_name} > trivy_image_vulnerabilities_results.json
+    sudo trivy -q image --light --timeout 30m -f cyclonedx ${image_name} > trivy_image_sbom_results.cyclonedx
 fi
 
 
