33154543ee46d1d5f74f75d9a4485d6c02a3b495
vishwajeet-ibm <vishwajeet.rajopadhye1@ibm.com>
2025-11-03 17:46:44 +0530

    GHA changes  (#63)
    * gha-new-changes
    
    * Update currency-build.yaml
    
    * Update currency-build.yaml
    
    * Update currency-build.yaml
    
    * Update upload_file.sh
    
    * Update upload_docker_image.sh
    
    * Update upload_wheel.sh
    
    * Update upload_docker_image.sh
    
    * Update currency-build.yaml
    
    * Update ca_config.py
    
    * Update ca_config.py
    
    * Update upload_file.sh
    
    * Update upload_file.sh
    
    * Update ca_config.py
    
    * Update upload_wheel.sh
    
    * Update upload_wheel.sh
    
    * Update ca_config.py
    
    * Update upload_docker_image.sh
    
    * Update COSWrapper.py
    
    * Update COSWrapper.py
    
    * Update currency-build.yaml
    
    * Update COSWrapper.py
    
    * Update upload_docker_image.sh
    
    * Update ca_config.py
    
    * Update COSWrapper.py
    
    * Update COSWrapper.py
    
    * Update ca_config.py
    
    ---------
    
    Co-authored-by: Vishwajeet-Rajopadhye1 <Vishwajeet-Rajopadhye1@ibm.com>

--- gha-script/upload-scripts/upload_docker_image.sh
+++ gha-script/upload-scripts/upload_docker_image.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -e
-
-echo "$travis_currency_service_id_api_key" | docker login -u iamapikey --password-stdin icr.io
+echo "$GHA_CURRENCY_SERVICE_ID_API_KEY" | docker login -u iamapikey --password-stdin icr.io
 if [ $? -ne 0 ]; then
     echo "Docker login failed. Exiting script."
     exit 1
 fi
 package_name=$(echo $PACKAGE_NAME | tr '[:upper:]' '[:lower:]')
-docker tag $IMAGE_NAME icr.io/ose4power-packages/$package_name-ppc64le:$VERSION
-docker push icr.io/ose4power-packages/$package_name-ppc64le:$VERSION
+docker tag $IMAGE_NAME icr.io/icr-ose4s390x-dev/$package_name-s390x:$VERSION
+docker push icr.io/icr-ose4s390x-dev/$package_name-s390x:$VERSION
 if [ $? -ne 0 ]; then
     echo "Docker push failed. Exiting script."
     exit 1
